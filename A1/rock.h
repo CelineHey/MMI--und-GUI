b1c185b58eff53df6f6b7cdcd4eff8e52bef3e28
jubo12 <jil.uboreck@stud.hs-ruhrwest.de>
2021-11-04 08:21:46 +0100

    update rock

--- A1/rock.h
+++ A1/rock.h
@@ -1,4 +1,21 @@
 #ifndef ROCK_H
 #define ROCK_H
+#include <stdbool.h>
+
+class Rock : public StaticGameEntity{
+
+private:
+    int m_sharpness;
+
+
+public:
+
+    Rock(int sharpness, bool destroy) : StaticGameEntity(destory){
+
+    }
+
+    int getSharpness();
+    void setSharpness(int sharpness);
+};
 
 #endif // ROCK_H
