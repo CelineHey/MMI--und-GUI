f67bb47dd618afed7b8519e3efd5ceb56f9947fa
jubo12 <jil.uboreck@stud.hs-ruhrwest.de>
2021-11-04 08:51:03 +0100

    update evilrock

--- A1/evilrock.h
+++ A1/evilrock.h
@@ -1,4 +1,13 @@
 #ifndef EVILROCK_H
 #define EVILROCK_H
 
+class EvilRock : public Rock, public Enemy{
+
+public:
+
+    EvilRock(int sharpness, double damagePerAttak) : Rock(sharpness) : Enemy(damagePerAttak += sharpness){
+
+    }
+};
+
 #endif // EVILROCK_H
