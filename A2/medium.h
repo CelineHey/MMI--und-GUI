5dc13c86fbdb3bdd2805db6472fde3140b1a731e
jubo12 <jil.uboreck@stud.hs-ruhrwest.de>
2021-11-05 10:57:10 +0100

    update library

--- A2/medium.h
+++ A2/medium.h
@@ -1,4 +1,19 @@
 #ifndef MEDIUM_H
 #define MEDIUM_H
+#include "library"
+
+class Medium : public Library{
+
+private:
+
+    char m_name[10];
+
+public:
+
+    Medium(char name[]) : Library(){
+
+    }
+
+};
 
 #endif // MEDIUM_H
