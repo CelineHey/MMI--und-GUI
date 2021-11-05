ba09f1a3e1765ba6999ac2091b244fc430e6f127
jubo12 <jil.uboreck@stud.hs-ruhrwest.de>
2021-11-05 11:02:10 +0100

    update book

--- A2/book.h
+++ A2/book.h
@@ -1,4 +1,21 @@
 #ifndef BOOK_H
 #define BOOK_H
+#include "medium"
+
+
+class Book : public Medium{
+
+private:
+
+
+    int m_numberOfBookPages;
+
+public:
+
+    Book(char name[], int numberOfBookPages) : Medium(name[]){
+
+    }
+
+};
 
 #endif // BOOK_H
